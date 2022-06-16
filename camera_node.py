f21bf9a9fba76fb6601167a19b108cf18ae71b27
GpeMireles <46947971+GpeMireles@users.noreply.github.com>
2022-06-16 00:00:16 -0500

    Update camera_node.py

--- camera_node.py
+++ camera_node.py
@@ -5,40 +5,45 @@ import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
+x = (
+  "nvarguscamerasrc sensor-id=%d !"
+  "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+  "nvvidconv flip-method=%d ! "
+  "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+  "videoconvert ! "
+  "video/x-raw, format=(string)BGR ! appsink"
+  % (
+      0,
+      1920,
+      1080,
+      30,
+      0,
+      960,
+      540,
+  )
+)
+video_capture = cv2.VideoCapture(x, cv2.CAP_GSTREAMER)
+
+
+
 class Camera:
   def __init__(self):
     self.bridge = CvBridge()
     self.image_pub = rospy.Publisher('/video', Image , queue_size=10)
+    self.img_msgs = None
 
   def show_camera(self):  
       # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
-      x = (
-        "nvarguscamerasrc sensor-id=%d !"
-        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            0,
-            1920,
-            1080,
-            30,
-            0,
-            960,
-            540,
-        )
-      )
-      video_capture = cv2.VideoCapture(x, cv2.CAP_GSTREAMER)
       ret, frame = video_capture.read()
+      if(not ret or frame is None):
+        return
       (h, w) = frame.shape[:2]
       (cX, cY) = (w // 2, h // 2)
       # rotate our img_msg by 45 degrees around the center of the img_msg
       M = cv2.getRotationMatrix2D((cX, cY), -180, 1.0)
       rotated = cv2.warpAffine(frame, M, (w, h))
       img_msg = self.bridge.cv2_to_imgmsg(rotated, "bgr8")
-      cv2.imshow("Rotated by -90 Degrees", rotated)
-      return img_msg
+      self.publish(img_msg)
 
   def publish(self, cap):
     try:
@@ -50,6 +55,7 @@ if __name__ == "__main__":
   obj = Camera()
   rospy.init_node('Camera',anonymous=True)
   while not rospy.is_shutdown():
-    img_msgs = obj.show_camera()
-    obj.publish(img_msgs)
+    obj.show_camera()
+    print("yes")
   print("Shutdown")
+  video_capture.release()
